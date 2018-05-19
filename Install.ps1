cb4f4aab9d809e5d8d471f928d5c6a3a84b753cd
Nicholas M. Getchell <getch3028@gmail.com>
2018-05-18 21:01:03 -0400

    10 minute timeout

--- Install.ps1
+++ Install.ps1
@@ -13,11 +13,14 @@ if ( $Inspect.State.Status -ne 'running' ) {
     docker run --detach --hostname gitlab.example.com --env GITLAB_OMNIBUS_CONFIG="external_url '$URI'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['initial_root_password'] = 'longterriblepassword!' "     --publish 4433:433 --publish 80:80 --publish 2222:22 --name $ContainerName --restart always gitlab/gitlab-ce:latest
 }
 
+Write-Host "Waiting for $ContainerName to start web server."
+$StartTime = Get-Date
 do {
-    Write-Host "Waiting for $ContainerName to start web server."
-    $StatusCode = docker exec -ti gitlab curl -s -o /dev/null -w "%{http_code}" localhost
     Start-Sleep -Seconds 5
-} while ( $StatusCode -ne 302 )
+    $StatusCode = docker exec -ti gitlab curl -s -o /dev/null -w "%{http_code}" localhost
+    $Timespent = (Get-Date).Subtract($StartTime)
+    "Time Elapsed: {0}" -f [math]::Round($Timespent.TotalSeconds)
+} while ( $StatusCode -ne 302  -or $Timespent.Minutes -gt 10 )
 Write-Host "Web server started and is properly redirecting."
 
 Write-Host "Setting Personal Access Token to $Token"
@@ -26,10 +29,5 @@ docker exec -ti -u git gitlab gitlab-rails r "token = PersonalAccessToken.new(us
 Install-Module PSGitLab -Scope CurrentUser -Force
 
 Save-GitLabAPIConfiguration -Token $Token -Domain 'http://localhost'
-
-
-
-Get-Gitlabuser -all
-
 
 Test-GitLabAPI
